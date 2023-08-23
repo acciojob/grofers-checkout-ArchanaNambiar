14fb55668b4145c47e00e329ea29af368bd430f6
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-23 20:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
-
 const getSum = () => {
-//Add your code here
-  
+  const priceElements = document.querySelectorAll(".price");
+  let totalPrice = 0;
+
+  priceElements.forEach((priceElement) => {
+    const price = parseFloat(priceElement.textContent);
+    if (!isNaN(price)) {
+      totalPrice += price;
+    }
+  });
+
+  const table = document.querySelector("table");
+  const newRow = table.insertRow(table.rows.length);
+  newRow.innerHTML = `<td>Total Price:</td><td>${totalPrice.toFixed(2)}</td>`;
 };
 
+const getSumBtn = document.createElement("button");
+getSumBtn.textContent = "Calculate Total";
 getSumBtn.addEventListener("click", getSum);
 
+document.body.appendChild(getSumBtn);
